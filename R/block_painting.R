4223d4ad4e55c46ec01bfa1a01d948e008cc4adf
Koen Derks <koen-derks@hotmail.com>
2021-04-14 15:36:28 +0200

    Update block_painting.R

--- R/block_painting.R
+++ R/block_painting.R
@@ -54,6 +54,9 @@ block_painting <- function(width, height, p.newcol, palette, seed){
   df <- reshape2::melt(df)
   colnames(df) <- c("y","x","z") # to name columns
   
+  df <- subset(df, df$x > 1 & df$x < max(df$x))
+  df <- subset(df, df$y > 1 & df$y < max(df$y))
+  
   painting <- ggplot2::ggplot(data = df, ggplot2::aes(x = x, y = y, fill = z)) + 
     geom_raster(interpolate = TRUE, alpha = 0.9) + 
     coord_equal() +
