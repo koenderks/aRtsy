6db7c3d37999734e38ef3c4aedc1a3cffe628774
Koen Derks <koen-derks@hotmail.com>
2021-08-07 13:29:03 +0200

    Update diamonds

--- R/paint_diamonds.R
+++ R/paint_diamonds.R
@@ -29,50 +29,49 @@
 
 paint_diamonds <- function(colors, background = '#fafafa', col.line = 'black', 
                            radius = 10, alpha = 1, size = 0.25, p = 0.2, width = 500, height = 500) {
-  x <- y <- xend <- yend <- NULL
-  artwork <- ggplot2::ggplot() +
-    ggplot2::xlim(c(0, width)) +
-    ggplot2::ylim(c(0, height)) + 
-    ggplot2::geom_curve(data = data.frame(x = 0, y = sample(0:height/2, size = 1), xend = width, yend = sample((height/2):height, size = 1)),
-                        mapping = ggplot2::aes(x = x, y = y, xend = xend, yend = yend),
-                        colour = col.line, size = stats::runif(1, 5, 15), curvature = stats::runif(1, 0, 0.5)) + 
-  ggplot2::geom_curve(data = data.frame(x = 0, y = sample((height/2):height, size = 1), xend = width, yend = sample(0:height/2, size = 1)),
-                      mapping = ggplot2::aes(x = x, y = y, xend = xend, yend = yend),
-                      colour = col.line, size = stats::runif(1, 5, 15), curvature = stats::runif(1, -0.5, 0))
+  x <- y <- xend <- yend <- type <- col <- NULL
   
   x <- seq(from = width / 5, to = width / 5 * 4, by = radius)
-  
   ymax <- seq(from = height / 2 + radius, to = height / 5 * 4, by = radius)
   ymax <- c(ymax, ymax[length(ymax)] + radius)
   ymax <- c(ymax, seq(from = ymax[length(ymax)] - radius, to = height / 2 + radius, by = -radius))
-  
   ymin <- seq(from = height / 2 - radius, to = height / 5, by = -radius)
   ymin <- c(ymin, ymin[length(ymin)] - radius)
   ymin <- c(ymin, seq(from = ymin[length(ymin)] + radius, to = height / 2 - radius, by = radius))
-  
   locs <- data.frame(x = x, ymin = ymin, ymax = ymax)
+  
   palette <- NULL
+  canvas <- data.frame(x = numeric(), y = numeric(), type = numeric())
   
   for (j in 1:nrow(locs)){
+    
     rs <- ceiling((ymax[j] - ymin[j]) / (radius * 2)) # required squares
-
+    
     for (i in 1:rs) {
+      
       xvec <- c(locs$x[j], locs$x[j] + radius, locs$x[j], locs$x[j] - radius, locs$x[j])
       yvec <- c(locs$ymax[j] - radius*((i-1)*2), (locs$ymax[j] - radius*((i-1)*2)) - radius, (locs$ymax[j] - radius*((i-1)*2)) - (radius*2), (locs$ymax[j] - radius*((i-1)*2)) - radius, locs$ymax[j] - radius*((i-1)*2))
-      d <- data.frame(x = xvec, y = yvec)
       
       col <- sample(c(NA, sample(colors, size = 1)), size = 1, prob = c(p, 1 - p))
-
-      if (!(col %in% palette)){
+      
+      if (!(col %in% palette))
         palette <- c(palette, col)
-      }
-        
-      artwork <- artwork + 
-        ggplot2::geom_polygon(data = d, mapping = ggplot2::aes(x = x, y = y), fill = col, color = background, alpha = alpha, size = size)
+      
+      diamond <- data.frame(x = xvec, y = yvec, type = paste0(j, i), col = col)
+      canvas <- rbind(canvas, diamond)
     }
   }
   
-  artwork <- artwork +
+  artwork <- ggplot2::ggplot(data = canvas, mapping = ggplot2::aes(x = x, y = y, group = type)) +
+    ggplot2::xlim(c(0, width)) +
+    ggplot2::ylim(c(0, height)) + 
+    ggplot2::geom_curve(data = data.frame(x = 0, y = sample(0:height/2, size = 1), xend = width, yend = sample((height/2):height, size = 1), type = 999 ),
+                        mapping = ggplot2::aes(x = x, y = y, xend = xend, yend = yend),
+                        colour = col.line, size = stats::runif(1, 5, 15), curvature = stats::runif(1, 0, 0.5)) + 
+    ggplot2::geom_curve(data = data.frame(x = 0, y = sample((height/2):height, size = 1), xend = width, yend = sample(0:height/2, size = 1), type = 999),
+                        mapping = ggplot2::aes(x = x, y = y, xend = xend, yend = yend),
+                        colour = col.line, size = stats::runif(1, 5, 15), curvature = stats::runif(1, -0.5, 0)) +
+    ggplot2::geom_polygon(fill = canvas$col, color = background, alpha = alpha, size = size) +
     ggplot2::scale_fill_manual(values = palette) +
     ggplot2::theme(axis.title = ggplot2::element_blank(),
                    axis.text = ggplot2::element_blank(),
