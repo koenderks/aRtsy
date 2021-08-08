d336e3a2b690ac121550059c2390487fc3232ad7
Koen Derks <koen-derks@hotmail.com>
2021-08-08 12:32:47 +0200

    Remove randomcolor package in workflows

--- .github/workflows/generate_artwork.R
+++ .github/workflows/generate_artwork.R
@@ -1,6 +1,5 @@
 # Packages required for artworks
 library(aRtsy)
-library(randomcoloR)
 
 # Name of the artwork
 filename <- paste0('png/daily.png')
@@ -14,12 +13,12 @@ type <- sample(1:10, size = 1)
 
 if (type == 1) {
   
-  artwork <- aRtsy::canvas_function(color = sample(c("black", randomcoloR::randomColor(1)), size = 1), 
+  artwork <- aRtsy::canvas_function(color = sample(c("black", aRtsy::colorPalette('random', 1)), size = 1), 
                                     background = sample(c("#fafafa", "#1a3657", "#343434", "#cc7722", "#a9d2c3", "#fc7c7c"), size = 1))
   
 } else if (type == 2) {
   
-  artwork <- aRtsy::canvas_strokes(colors = randomcoloR::randomColor(count = sample(5:15, size = 1)),
+  artwork <- aRtsy::canvas_strokes(colors = aRtsy::colorPalette('random', sample(5:15, size = 1)),
                                    neighbors = sample(1:4, size = 1),
                                    p = runif(1, 0.0001, 0.01),
                                    iter = sample(1:3, size = 1),
@@ -29,8 +28,8 @@ if (type == 1) {
   
 } else if (type == 3) {
   
-  artwork <- aRtsy::canvas_turmite(color = sample(c("#000000", randomColor(count = 1)), size = 1),
-                                   background = sample(c("#fafafa", "#cc7722", "#a9d2c3", "#fc7c7c", randomColor(count = 1)), size = 1),
+  artwork <- aRtsy::canvas_turmite(color = sample(c("#000000", aRtsy::colorPalette('random', 1)), size = 1),
+                                   background = sample(c("#fafafa", "#cc7722", "#a9d2c3", "#fc7c7c", aRtsy::colorPalette('random', 1)), size = 1),
                                    p = runif(1, 0.2, 0.5),
                                    iterations = 1e7,
                                    width = 1500, 
@@ -39,15 +38,15 @@ if (type == 1) {
 } else if (type == 4) {
   
   dims <- sample(c(500, 1000, 1500), size = 1)
-  artwork <- aRtsy::canvas_ant(colors = randomcoloR::randomColor(count = sample(6:20, size = 1)),
-                               background = sample(c("#fafafa", "#cc7722", "#a9d2c3", "#fc7c7c", randomColor(count = 1)), size = 1),
+  artwork <- aRtsy::canvas_ant(colors = aRtsy::colorPalette('random', sample(6:20, size = 1)),
+                               background = sample(c("#fafafa", "#cc7722", "#a9d2c3", "#fc7c7c", aRtsy::colorPalette('random', 1)), size = 1),
                                iterations = 1e7,
                                width = dims, 
                                height = dims)
   
 } else if (type == 5) {
   
-  artwork <- aRtsy::canvas_mondriaan(colors = randomcoloR::randomColor(count = sample(3:10, size = 1)),
+  artwork <- aRtsy::canvas_mondriaan(colors = aRtsy::colorPalette('random', sample(3:10, size = 1)),
                                      background = '#000000',
                                      cuts = sample(10:200, size = 1),
                                      ratio = 1.618,
@@ -56,13 +55,13 @@ if (type == 1) {
   
 } else if (type == 6) {
   
-  artwork <- aRtsy::canvas_planet(colors = list(randomcoloR::randomColor(3)), 
+  artwork <- aRtsy::canvas_planet(colors = list(aRtsy::colorPalette('random', 3)), 
                                   iterations = 30, 
                                   starprob = runif(1, 0.001, 0.1))
   
 } else if (type == 7) {
   
-  artwork <- aRtsy::canvas_circlemap(colors = randomcoloR::randomColor(3, luminosity = "dark"),
+  artwork <- aRtsy::canvas_circlemap(colors = aRtsy::colorPalette('random', 3),
                                      x_min = runif(1, -4, 0),
                                      x_max = runif(1, 1, 14),
                                      y_min = 0,
@@ -73,23 +72,23 @@ if (type == 1) {
   
 } else if (type == 8) {
   
-  artwork <- aRtsy::canvas_ribbons(colors = randomcoloR::randomColor(sample(3:6, size = 1), luminosity = "dark"),
-                                   background = randomcoloR::randomColor(1, luminosity = "light"))
+  artwork <- aRtsy::canvas_ribbons(colors = aRtsy::colorPalette('random', sample(3:6, size = 1)),
+                                   background = aRtsy::colorPalette('random', 1))
   
 } else if (type == 9) {
   
-  artwork <- aRtsy::canvas_polylines(colors = randomcoloR::randomColor(sample(3:6, size = 1), luminosity = "dark"),
-                                     background = sample(c("#fafafa", "black", randomcoloR::randomColor(1, luminosity = "light")), size = 1))
+  artwork <- aRtsy::canvas_polylines(colors = aRtsy::colorPalette('random', sample(3:6, size = 1)),
+                                     background = sample(c("#fafafa", "black", aRtsy::colorPalette('random', 1)), size = 1))
   
 } else if (type == 10) {
   
-  artwork <- aRtsy::canvas_diamonds(colors = randomcoloR::randomColor(sample(4:8, size = 1)), 
-                                    background = sample(c("#fafafa", "black", randomcoloR::randomColor(1, luminosity = "light")), size = 1),
-                                    col.line = sample(c("#fafafa", "black", randomcoloR::randomColor(1, luminosity = "dark")), size = 1),
+  artwork <- aRtsy::canvas_diamonds(colors = aRtsy::colorPalette('random', sample(4:8, size = 1)), 
+                                    background = sample(c("#fafafa", "black", aRtsy::colorPalette('random', 1)), size = 1),
+                                    col.line = sample(c("#fafafa", "black", aRtsy::colorPalette('random', 1)), size = 1),
                                     radius = sample(c(2, 2.5, 5, 7, 7.5, 10), size = 1),
                                     p = sample(seq(0, 0.7, 0.1), size = 1),
                                     size = sample(c(0, 0.2), size = 1))
   
 }
 
-ggplot2::ggsave(artwork, filename = filename, width = 7, height = 7, dpi = 300)
+aRtsy::saveCanvas(artwork, filename)
