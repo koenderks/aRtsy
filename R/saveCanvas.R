2e911104dc242b7c57eb5ae7f613a495e493d579
Koen Derks <koen-derks@hotmail.com>
2021-08-27 01:37:07 +0200

    Missing dpi for cran check

--- R/saveCanvas.R
+++ R/saveCanvas.R
@@ -2,7 +2,7 @@
 #'
 #' @description This function is a wrapper around \code{ggplot2::ggsave}. It provides a suggested export with square dimensions for a canvas created using the \code{aRtsy} package.
 #'
-#' @usage saveCanvas(plot, filename, width = 7, height = 7, resolution = 300)
+#' @usage saveCanvas(plot, filename, width = 7, height = 7, dpi = 300)
 #'
 #' @param plot       a ggplot2 object to be saved.
 #' @param filename   the filename of the export.
