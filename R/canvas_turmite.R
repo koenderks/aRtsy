0925d0343d5779aae2f93d93d40218eb2b9bd7f9
Koen Derks <koen-derks@hotmail.com>
2021-08-29 00:02:32 +0200

    Add noise to turmite

--- R/canvas_turmite.R
+++ R/canvas_turmite.R
@@ -2,15 +2,16 @@
 #'
 #' @description This function paints a turmite. A turmite is a Turing machine which has an orientation in addition to a current state and a "tape" that consists of a two-dimensional grid of cells. The algorithm is simple: 1) turn on the spot (left, right, up, down) 2) change the color of the square 3) move forward one square.
 #'
-#' @usage canvas_turmite(color, background = '#fafafa', p = 0.5, iterations = 1e7, 
-#'                width = 1500, height = 1500)
+#' @usage canvas_turmite(colors, background = '#fafafa', p = 0.5, iterations = 1e7, 
+#'                width = 1500, height = 1500, noise = 'none')
 #'
-#' @param color       a character specifying the color used for the artwork.
+#' @param colors       a character specifying the color used for the artwork.
 #' @param background  a character specifying the color used for the background.
 #' @param p           a value specifying the probability of a state switch within the turmite.
 #' @param iterations  a positive integer specifying the number of iterations of the algorithm.
 #' @param width       a positive integer specifying the width of the artwork in pixels.
 #' @param height      a positive integer specifying the height of the artwork in pixels.
+#' @param noise       a character specifying the type of noise to add to the turmite. Can be one of \code{none} (the default), \code{knn} for k-nearest neighbors noise, \code{svm} for support vector machine noise and \code{rf} for random forest noise.
 #'
 #' @references \url{https://en.wikipedia.org/wiki/Turmite}
 #'
@@ -21,7 +22,8 @@
 #' @examples
 #' \donttest{
 #' set.seed(17)
-#' canvas_turmite(color = '#000000')
+#' palette <- colorPalette('dark2')
+#' canvas_turmite(colors = palette, p = 0, noise = "knn")
 #' }
 #' 
 #' @keywords artwork canvas
@@ -30,23 +32,30 @@
 #' @useDynLib aRtsy
 #' @import Rcpp
 
-canvas_turmite <- function(color, background = '#fafafa', p = 0.5, iterations = 1e7, 
-                           width = 1500, height = 1500) {
+canvas_turmite <- function(colors, background = '#fafafa', p = 0.5, iterations = 1e7, 
+                           width = 1500, height = 1500, noise = 'none') {
   x <- y <- z <- NULL
-  if (length(color) > 1)
-    stop("This artwork can only take one color value.")
   if (length(background) > 1)
     stop("This artwork can only take one background value.")
-  palette <- c(background, color)
-  k <- sample(0:1, size = 1)
-  row <- 0
-  col <- 0
-  if (k == 1)
-    col <- sample(0:(width-1), size = 1)
-  if (k == 0)
-    row <- sample(0:(height-1), size = 1) 
-  full_canvas <- draw_turmite(matrix(0, nrow = height, ncol = width), iterations, row, col, p = p)
-  full_canvas <- unraster(full_canvas, names = c('y', 'x', 'z')) # Convert 2D matrix to data frame
+  palette <- c(background, colors)
+  canvas <- matrix(0, nrow = height, ncol = width)
+  for (i in 1:length(colors)) {
+    k <- sample(0:1, size = 1)
+    row <- 0
+    col <- 0
+    if (k == 1)
+      col <- sample(0:(width-1), size = 1)
+    if (k == 0)
+      row <- sample(0:(height-1), size = 1)
+    turmite <- canvas + (i - 1) + draw_turmite(matrix(0, nrow = height, ncol = width), iterations, row, col, p = p)
+    if (noise != "none") {
+      turmite[which(turmite == 0)] <- NA
+      turmite <- turmite - noise(dims = c(height, width), type = noise)
+      turmite[which(is.na(turmite))] <- 0
+    }
+    canvas <- canvas + turmite
+  }
+  full_canvas <- unraster(canvas, names = c('x', 'y', 'z')) # Convert 2D matrix to data frame
   artwork <- ggplot2::ggplot(data = full_canvas, ggplot2::aes(x = x, y = y, fill = z)) +
     ggplot2::geom_raster(interpolate = TRUE, alpha = 0.9) + 
     ggplot2::coord_equal() +
--- R/internal.R
+++ R/internal.R
@@ -4,3 +4,27 @@ unraster <- function(x, names) {
   colnames(newx) <- names
   return(newx)
 }
+
+noise <- function(dims, type = "knn", n = 100) {
+  if (length(dims) == 1) {
+    vec <- expand.grid(0, seq(0, 1, length.out = dims)) # Create all combinations of pixels
+  } else if (length(dims) == 2) {
+    vec <- expand.grid(seq(0, 1, length.out = dims[1]), seq(0, 1, length.out = dims[2])) # Create all combinations of pixels
+  }
+  colnames(vec) <- c("x", "y")
+  train <- data.frame(x = stats::runif(n, 0, 1), y = stats::runif(n, 0, 1), z = stats::runif(n, 0, 1))
+  if (type == 'knn') {
+    fit <- kknn::train.kknn(formula = z ~ x + y, data = train) # Fit knn model to training data
+  } else if (type == "svm") {
+    fit <- e1071::svm(formula = z ~ x + y, data = train)
+  } else if (type == 'rf') {
+    fit <- randomForest::randomForest(formula = z ~ x + y, data = train)
+  }
+  z <- predict(fit, newdata = vec) # Predict each pixel using the fitted model
+  if (length(dims) == 1) {
+    newx <- z
+  } else if (length(dims) == 2) {
+    newx <- matrix(z, nrow = dims[1], ncol = dims[2])
+  }
+  return(newx)
+}
