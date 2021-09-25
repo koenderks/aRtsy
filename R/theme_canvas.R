64a93e662d09831fd2d141dcb85f463d27dd1a0f
Koen Derks <koen-derks@hotmail.com>
2021-09-25 20:28:16 +0200

    Update background handling

--- R/theme_canvas.R
+++ R/theme_canvas.R
@@ -2,7 +2,7 @@
 #'
 #' @description Add a canvas theme to the plot. The canvas theme by default has no margins and fills any empty canvas with a background color.
 #'
-#' @usage theme_canvas(x, background = 'white', margin = 0)
+#' @usage theme_canvas(x, background = NULL, margin = 0)
 #'
 #' @param x            a ggplot2 object.
 #' @param background   a character specifying the color used for the empty canvas.
@@ -16,20 +16,24 @@
 #'
 #' @export
 
-theme_canvas <- function(x, background = "white", margin = 0) {
+theme_canvas <- function(x, background = NULL, margin = 0) {
   x <- x + ggplot2::theme(
     axis.title = ggplot2::element_blank(),
     axis.text = ggplot2::element_blank(),
     axis.ticks = ggplot2::element_blank(),
     axis.line = ggplot2::element_blank(),
     legend.position = "none",
-    panel.background = ggplot2::element_rect(fill = background, colour = background),
+	plot.background = ggplot2::element_rect(fill = background, colour = background),
     panel.border = ggplot2::element_blank(),
     panel.grid = ggplot2::element_blank(),
-    plot.background = ggplot2::element_rect(fill = background, colour = background),
     plot.margin = ggplot2::unit(rep(margin, 4), "lines"),
     strip.background = ggplot2::element_blank(),
     strip.text = ggplot2::element_blank()
   )
+  if (is.null(background)) {
+	x <- x + ggplot2::theme(panel.background = ggplot2::element_blank())
+  } else {
+	x <- x + ggplot2::theme(panel.background = ggplot2::element_rect(fill = background, colour = background))
+  }
   return(x)
 }
