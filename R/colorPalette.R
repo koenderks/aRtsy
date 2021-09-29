e872486c9d03d051e8b9668449f40d92ca869ac9
Koen Derks <koen-derks@hotmail.com>
2021-09-29 10:08:53 +0200

    Use hsl for random colors instead of rgb

--- R/colorPalette.R
+++ R/colorPalette.R
@@ -30,7 +30,10 @@ colorPalette <- function(name, n = NULL) {
     if (is.null(n)) {
       stop("'n' is missing for palette = 'random'")
     }
-    palette <- grDevices::rgb(stats::runif(n, 0, 255), stats::runif(n, 0, 255), stats::runif(n, 0, 255), maxColorValue = 255)
+    palette <- character(n)
+    for (i in 1:length(palette)) {
+      palette[i] <- .hsl_to_rgb(h = stats::runif(1, 0, 360), stats::runif(1), stats::runif(1))
+    }
   } else {
     palette <- switch(name,
       "blackwhite" = c("black", "white"),
@@ -64,4 +67,39 @@ colorPalette <- function(name, n = NULL) {
     palette <- palette[1:n]
   }
   return(palette)
+}
+
+.hsl_to_rgb <- function(h, s, l) {
+  h <- h / 360
+  r <- g <- b <- 0.0
+  if (s == 0) {
+    r <- g <- b <- l
+  } else {
+    q <- ifelse(l < 0.5, l * (1.0 + s), l + s - (l * s))
+    p <- 2.0 * l - q
+    r <- .hue_to_rgb(p, q, h + 1 / 3)
+    g <- .hue_to_rgb(p, q, h)
+    b <- .hue_to_rgb(p, q, h - 1 / 3)
+  }
+  col <- grDevices::rgb(r, g, b)
+  return(col)
+}
+
+.hue_to_rgb <- function(p, q, t) {
+  if (t < 0) {
+    t <- t + 1.0
+  }
+  if (t > 1) {
+    t <- t - 1.0
+  }
+  if (t < 1 / 6) {
+    return(p + (q - p) * 6.0 * t)
+  }
+  if (t < 1 / 2) {
+    return(q)
+  }
+  if (t < 2 / 3) {
+    return(p + ((q - p) * ((2 / 3) - t) * 6))
+  }
+  return(p)
 }
