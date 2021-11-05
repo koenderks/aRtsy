fd38d304c14051e2c9b72020592a2952ac2c64fe
Koen Derks <koen-derks@hotmail.com>
2021-11-05 09:14:29 +0100

    Add closed argument to canvas_recaman

--- R/canvas_recaman.R
+++ R/canvas_recaman.R
@@ -3,7 +3,7 @@
 #' @description This function draws Recaman's sequence on a canvas. The algorithm takes increasingly large steps backward on the positive number line, but if it is unable to it takes a step forward.
 #'
 #' @usage canvas_recaman(colors, background = "#fafafa", n = 100, start = 0,
-#'                increment = 1, curvature = 1, angle = 0)
+#'                increment = 1, curvature = 1, angle = 0, closed = FALSE)
 #'
 #' @param colors         a string or character vector specifying the color(s) used for the artwork.
 #' @param background     a character specifying the color used for the background.
@@ -12,6 +12,7 @@
 #' @param increment      the increment of each step.
 #' @param curvature      the curvature of each line.
 #' @param angle          the angle at which to place the artwork.
+#' @param closed         logical. Whether to plot a curve from the end of the sequence back to the starting point.
 #'
 #' @return A \code{ggplot} object containing the artwork.
 #'
@@ -34,7 +35,7 @@
 #' @export
 
 canvas_recaman <- function(colors, background = "#fafafa", n = 100, start = 0,
-                           increment = 1, curvature = 1, angle = 0) {
+                           increment = 1, curvature = 1, angle = 0, closed = FALSE) {
   .checkUserInput(background = background)
   if (!(angle %in% c(0, 45))) {
     stop("'angle' must be either 0 or 45 degrees")
@@ -60,13 +61,19 @@ canvas_recaman <- function(colors, background = "#fafafa", n = 100, start = 0,
       xlist <- c(xlist, x[i - 1] - inc)
     }
   }
-  canvas <- data.frame(id = 1:length(x), x = x, xend = c(x[-1], x[1]))
+  if (closed) {
+    xend <- c(x[-1], x[1])
+  } else {
+    xend <- x[-1]
+    x <- x[-length(x)]
+  }
+  canvas <- data.frame(id = 1:length(x), x = x, xend = xend)
   canvas$y <- x * sin(angle%%360)
   canvas$yend <- canvas$y[match(canvas$xend, canvas$x)]
-  minx <- apply(canvas[, 2:3], 1, min)
-  maxx <- apply(canvas[, 2:3], 1, max)
-  miny <- apply(canvas[, 4:5], 1, min)
-  maxy <- apply(canvas[, 4:5], 1, max)
+  minx <- apply(canvas[, 2:3], 1, min, na.rm = TRUE)
+  maxx <- apply(canvas[, 2:3], 1, max, na.rm = TRUE)
+  miny <- apply(canvas[, 4:5], 1, min, na.rm = TRUE)
+  maxy <- apply(canvas[, 4:5], 1, max, na.rm = TRUE)
   for (i in 1:nrow(canvas)) {
     if (i %% 2 == 1) {
       canvas[i, 2:5] <- c(minx[i], maxx[i], miny[i], maxy[i])
